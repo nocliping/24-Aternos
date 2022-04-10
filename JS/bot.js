5f4392e2fbde354026c342be46fe2e1015df795c
nocliping <103381564+nocliping@users.noreply.github.com>
2022-04-10 21:37:04 +0300

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,10 +4,10 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "Test",
-    version: false
+    host: "Chmarted.aternos.me",
+    port: "22722",
+    username: "AdrianSEXY",
+    version: 1.18.1
     })
     bot.on('login', function() {
       bot.chat('/reginster 123123123 123123123')
